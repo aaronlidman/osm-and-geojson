90dfb4dbe0f78903dd7513b06dd8122c970078b9
Aaron Lidman <aaronlidman@gmail.com>
2013-09-14 14:58:16 -0700

    Add osmChange option

--- osm_geojson.js
+++ osm_geojson.js
@@ -1,6 +1,6 @@
 var osm_geojson = {};
 
-osm_geojson.geojson2osm = function(geo, changeset) {
+osm_geojson.geojson2osm = function(geo, changeset, osmChange) {
     function togeojson(geo, properties) {
         var nodes = '',
             ways = '',
@@ -56,6 +56,10 @@ osm_geojson.geojson2osm = function(geo, changeset) {
 
         osm = '<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="github.com/aaronlidman/osm-and-geojson">' +
         nodes + ways + relations + '</osm>';
+        if (osmChange) {
+            osm = '<osmChange version="0.6" generator="github.com/aaronlidman/osm-and-geojson"><create>' +
+            nodes + ways + relations + '</create></osmChange>';
+        }
 
         return {
             nodes: nodes,
@@ -181,13 +185,18 @@ osm_geojson.geojson2osm = function(geo, changeset) {
                 obj.push(togeojson(geo.features[i].geometry, geo.features[i].properties));
             }
             temp.osm = '<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="github.com/aaronlidman/osm-and-geojson">';
+            if (osmChange) temp.osm = '<osmChange version="0.6" generator="github.com/aaronlidman/osm-and-geojson"><create>';
             for (var n = 0; n < obj.length; n++) {
                 temp.nodes += obj[n].nodes;
                 temp.ways += obj[n].ways;
                 temp.relations += obj[n].relations;
             }
             temp.osm += temp.nodes + temp.ways + temp.relations;
-            temp.osm += '</osm>';
+            if (osmChange) {
+                temp.osm += '</create></osmChange>';
+            } else {
+                temp.osm += '</osm>';
+            }
             obj = temp.osm;
             break;
 
