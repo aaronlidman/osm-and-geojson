f2249746a095d5c714464c4b4cf38f002cab2459
Rub21 <ruben@mapbox.com>
2015-03-12 14:34:17 -0500

    Fixing for polygons and linestring

--- README.md
+++ README.md
@@ -1,26 +1,12 @@
-## OSM & GeoJSON
-
-- Converts [OSM XML](http://wiki.openstreetmap.org/wiki/OSM_XML) to [GeoJSON](http://www.geojson.org/)
-- Also converts the inverse, GeoJSON to OSM XML
-- This is a fork of [OSM2GEO by tecoholic](https://gist.github.com/tecoholic/1396990) with some improvements
-    - vanilla JS, no dependencies
-    - multipolygon support
-    - browser or nodejs
-- __warning:__ When converting GeoJSON to OSM XML only points, polygons, and multipolygons (standalone or in feature collections) are supported right now
+## GeoJSON -> OSM
 
 ## Usage
-- for the browser
-    - `<script src='osm_geojson.js'></script>`
 - for nodejs
-    - `npm install osm-and-geojson`
+    - `npm install geojson2osm`
 
-## API
+Example:
+```js
 
-- ####`osm_geojson.osm2geojson(osmXmlStringOrDOM)`
-    - Parse and convert a string of OSM XML to a GeoJSON object. Add an optional second argument of `true`, `osm_geojson.osm2geojson(yourOsm, true)` to include metadata about the item in the properties, namespaced `osm_*`.
-
-- ####`osm_geojson.geojson2osm(GeoJSONObject)`
-    - Parse and convert a GeoJSON object into OSM XML.
-
----
-OSM & GeoJSON was written in the [Orange Public Library](http://www.flickr.com/photos/twobears2/5163373046/in/set-72157625352078650). Always upstairs, the window seats are awesome but really tough to get. More often than not I'd get stuck with one of the middle tables where foot traffic was a bit much and outlets are hard to come by.
+var osm_geojson = require('geojson2osm');
+osm_geojson.geojson2osm(json);
+```
---
+++ index.js
@@ -0,0 +1,193 @@
+var geojson2osm = {};
+geojson2osm.geojson2osm = function(geojson) {
+    function togeojson(geo, properties) {
+        if (typeof geo === 'string') geo = JSON.parse(geo);
+        var nodes = '',
+            ways = '',
+            relations = '';
+        properties = properties || {};
+        switch (geo.type) {
+            case 'Point':
+                append(Point(geo, properties));
+                break;
+            case 'MultiPoint':
+                break;
+            case 'LineString':
+                append(LineString(geo, properties)); //if polygon is made with LineString,this working too.
+                break;
+            case 'MultiLineString':
+                append(MultiLineString(geo, properties));
+                break;
+            case 'Polygon':
+                append(Polygon(geo, properties));
+                break;
+            case 'MultiPolygon':
+                break;
+        }
+
+        function append(obj) {
+            nodes += obj.nodes;
+            ways += obj.ways;
+            relations += obj.relations;
+        }
+        return {
+            nodes: nodes,
+            ways: ways,
+            relations: relations
+        };
+    }
+
+    function Point(geo, properties) {
+        var nodes = '';
+        var coord = roundCoords([geo.coordinates]);
+        nodes += '<node id="' + count + '" lat="' + coord[0][1] + '" lon="' + coord[0][0] + '" changeset="' + changeset + '">';
+        nodes += propertiesToTags(properties);
+        nodes += '</node>';
+        count--;
+        return {
+            nodes: nodes
+        };
+    }
+
+    function LineString(geo, properties) {
+        var nodes = '',
+            ways = '';
+        var coords = [];
+        ways += '<way id="' + count + '" changeset="' + changeset + '">';
+        count--;
+        for (var i = 0; i <= geo.coordinates.length - 1; i++) {
+            coords.push([geo.coordinates[i][1], geo.coordinates[i][0]]);
+        }
+        coords = createNodes(coords, false);
+        nodes += coords.nodes;
+        ways += coords.nds;
+        ways += propertiesToTags(properties);
+        ways += '</way>';
+        return {
+            nodes: nodes,
+            ways: ways
+        };
+    }
+
+    function MultiLineString(geo, properties) {
+        var nodes = '',
+            ways = '';
+
+        var coords = [];
+        ways += '<way id="' + count + '" changeset="' + changeset + '">';
+        count--;
+
+        for (var i = 0; i <= geo.coordinates[0].length - 1; i++) {
+            coords.push([geo.coordinates[0][i][1], geo.coordinates[0][i][0]]);
+        }
+        coords = createNodes(coords, false);
+        nodes += coords.nodes;
+        ways += coords.nds;
+        ways += propertiesToTags(properties);
+        ways += '</way>';
+        return {
+            nodes: nodes,
+            ways: ways
+        };
+    }
+
+    function Polygon(geo, properties) {
+        var nodes = '',
+            ways = '';
+        var coords = [];
+        ways += '<way id="' + count + '" changeset="' + changeset + '">';
+        count--;
+
+        for (var i = 0; i <= geo.coordinates[0].length - 1; i++) {
+            coords.push([geo.coordinates[0][i][1], geo.coordinates[0][i][0]]);
+        }
+        coords = createNodes(coords, false);
+        nodes += coords.nodes;
+        ways += coords.nds;
+        ways += propertiesToTags(properties);
+        ways += '</way>';
+        return {
+            nodes: nodes,
+            ways: ways
+        };
+    }
+
+    function propertiesToTags(properties) {
+        var tags = '';
+        for (var tag in properties) {
+            if (properties[tag] !== null) {
+                tags += '<tag k="' + tag + '" v="' + properties[tag] + '"/>';
+            }
+        }
+        return tags;
+    }
+
+    function roundCoords(coords) {
+        for (var a = 0; a < coords.length; a++) {
+            coords[a][0] = Math.round(coords[a][0] * 1000000) / 1000000;
+            coords[a][1] = Math.round(coords[a][1] * 1000000) / 1000000;
+        }
+        return coords;
+    }
+
+    function createNodes(coords, repeatLastND) {
+        var nds = '',
+            nodes = '',
+            length = coords.length;
+        repeatLastND = repeatLastND || false;
+        coords = roundCoords(coords);
+        for (var a = 0; a < length; a++) {
+            if (hash.hasOwnProperty(coords[a])) {
+                nds += '<nd ref="' + hash[coords[a]] + '"/>';
+            } else {
+                hash[coords[a]] = count;
+                if (repeatLastND && a === 0) {
+                    repeatLastND = count;
+                }
+                nds += '<nd ref="' + count + '"/>';
+                nodes += '<node id="' + count + '" lat="' + coords[a][0] + '" lon="' + coords[a][1] + '" changeset="' + changeset + '"/>';
+
+                if (repeatLastND && a === length - 1) {
+                    nds += '<nd ref="' + repeatLastND + '"/>';
+                }
+            }
+            count--;
+        }
+        return {
+            'nds': nds,
+            'nodes': nodes
+        };
+    }
+    var hash = {};
+    var count = -1;
+    var changeset = 'false';
+    var osm_file = '';
+    if (typeof geojson === 'string') geojson = JSON.parse(geojson);
+    switch (geojson.type) {
+        case 'FeatureCollection':
+            var temp = {
+                nodes: '',
+                ways: '',
+                relations: ''
+            };
+            obj = [];
+            for (var i = 0; i < geojson.features.length; i++) {
+                obj.push(togeojson(geojson.features[i].geometry, geojson.features[i].properties));
+            }
+            for (var n = 0; n < obj.length; n++) {
+                temp.nodes += obj[n].nodes;
+                temp.ways += obj[n].ways;
+                temp.relations += obj[n].relations;
+            }
+            temp.osm = '<?xml version="1.0" encoding="UTF-8"?><osm version="0.6" generator="Rub21">';
+            temp.osm += temp.nodes + temp.ways + temp.relations;
+            temp.osm += '</osm>';
+            osm_file = temp.osm;
+            break;
+        default:
+            console.log('default');
+            break;
+    }
+    return osm_file;
+};
+if (typeof module !== 'undefined') module.exports = geojson2osm;
