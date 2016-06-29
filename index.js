68bff87a25c32aeb6ac032972ff59767e13e204f
Rub21 <ruben@mapbox.com>
2016-06-29 15:10:37 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
-
 'use strict';
 var fs = require('fs');
-
 var geojson2osm = require('./src/geojson2osm');
 var file = process.argv.slice(2)[0];
 var contents = fs.readFileSync(file).toString();
-// geojson2osm.geojson2osm(contents)
- process.stdout.write(geojson2osm.geojson2osm(contents));
+process.stdout.write(geojson2osm.geojson2osm(contents));
