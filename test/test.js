d690dd89943467d9fa4edf2e74a2e5a48ff75703
Rub21 <ruben@mapbox.com>
2016-07-01 10:24:55 -0500

    testting

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var tape = require('tape');
+var path = require('path');
+var geojson2osm = require('../index.js');
+
+tape('geojson(osmlint output) to osm  -', function(assert) {
+  var geojsonFile = path.join(__dirname, '/fixtures/ramyaragupathy.geojson');
+  var osmFile = path.join(__dirname, '/fixtures/ramyaragupathy.osm');
+  var geojson = fs.readFileSync(geojsonFile).toString();
+  var osm = fs.readFileSync(osmFile).toString();
+  var outOSM = geojson2osm.geojson2osm(geojson);
+  assert.equal(geojson2osm.geojson2osm(geojson), osm, 'Correct ramyaragupathy.geojson OSM output');
+  assert.end();
+});
+
+tape('geojson(from geojson.io) to osm  -', function(assert) {
+  var geojsonFile = path.join(__dirname, '/fixtures/ayacucho.geojson');
+  var osmFile = path.join(__dirname, '/fixtures/ayacucho.osm');
+  var geojson = fs.readFileSync(geojsonFile).toString();
+  var osm = fs.readFileSync(osmFile).toString();
+  var outOSM = geojson2osm.geojson2osm(geojson);
+  assert.equal(geojson2osm.geojson2osm(geojson), osm, 'Correct ayacucho.geojson OSM output');
+  assert.end();
+});
